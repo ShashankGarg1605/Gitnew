2aae8160edc46267d069680e31badbb1602c5429
Abhishek Jain <abhishek.jain@prozo.com>
2017-08-31 13:12:34 +0530

    left menu design improv

--- src/components/LeftPanel.vue
+++ src/components/LeftPanel.vue
@@ -1,14 +1,45 @@
 <template>
   <f7-panel left :reveal="isiOS" :cover="isMaterial">
-    <f7-navbar title="Menu"></f7-navbar>
-    <f7-page>
-      <f7-block-title>Links</f7-block-title>
-      <f7-list>
-        <f7-list-item @click="signOut()" title="Sign Out" class="close-panel" />
-        <f7-list-item link="/dashboard" title="Dashboard" link-view="#main-view" link-reload link-close-panel />
-        <f7-list-item link="/allorders/" title="All Orders" link-view="#main-view" link-reload link-close-panel />
-      </f7-list>
-    </f7-page>
+
+    <div class="page pz-padding-0 pz-margin-0">
+      <f7-navbar title="Menu"></f7-navbar>
+      <div class="page-content pz-width100 pz-padding-t44" style="display: flex; flex-direction: column; ">
+
+        <div class="list-block">
+          <ul>
+            <a class="item-content close-panel button pz-colr-inherit pz-cap" data-reload="true" href="/dashboard">
+              <div class="item-media">
+                <icon name="line-chart"></icon>
+              </div>
+              <div class="item-inner pz-margin-l0">
+                <div class="item-title">Dashboard</div>
+              </div>
+            </a>
+            <a class="item-content close-panel button pz-colr-inherit pz-cap" data-reload="true" href="/allorders">
+              <div class="item-media">
+                <icon name="shopping-cart"></icon>
+              </div>
+              <div class="item-inner pz-margin-l0">
+                <div class="item-title">All Orders</div>
+              </div>
+            </a>
+            <a class="item-content close-panel button pz-colr-inherit pz-cap" data-reload="true" href="/purchaseinvoice">
+              <div class="item-media">
+                <icon name="table"></icon>
+              </div>
+              <div class="item-inner pz-margin-l0">
+                <div class="item-title">Purchase Invoice</div>
+              </div>
+            </a>
+          </ul>
+        </div>
+
+        <div class="pz-height100 pz-flex-c-e pz-margin-b30">
+          <a href="#" class="button button-raised close-panel" @click="signOut()">Sign Out</a>
+        </div>
+
+      </div>
+    </div>
   </f7-panel>
 </template>
 
@@ -34,11 +65,6 @@ export default {
 </script>
 
 <style scoped>
-.panel-left .page {
-  margin-top: 44px;
-  padding-bottom: 44px;
-}
-
 @media (min-width: 960px) {
   .panel.panel-left {
     border-right: 1px solid #b2b2b2;

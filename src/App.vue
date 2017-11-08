39860d1812ea03a5ba108acaec5f2f94e8b9dc13
Abhishek Jain <abhishek.jain@prozo.com>
2017-11-08 17:04:55 +0530

    open dashboard by default

--- src/App.vue
+++ src/App.vue
@@ -32,7 +32,7 @@ export default {
   },
   methods: {
     defaultPage() {
-      return window.localStorage.authToken ? 'allorders' : '/';
+      return window.localStorage.authToken ? 'dashboard' : '/';
     }
   },
   computed: {

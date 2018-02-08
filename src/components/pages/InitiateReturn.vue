434f9e9d8c13586af61673f69efe8ddfd80ab6bc
Abhishek Jain <abhishek.jain@prozo.com>
2018-02-08 16:07:35 +0530

    new user selection component that loads user data and then allows selection of one user
    initiate return migrated to this component
    
    will migrate other pages as well once testing for returns page is passed

--- src/components/pages/InitiateReturn.vue
+++ src/components/pages/InitiateReturn.vue
@@ -10,19 +10,9 @@
             </f7-nav-center>
         </f7-navbar>
 
-        <section v-if="users">
-            <div class="list-block pz-margin-top0">
-                <a href="#" id="autocomplete-standalone-popup-2" class="item-link item-content autocomplete-opener" @click="openUserSelection()">
-                    <input type="hidden" v-model="userID">
-                    <div class="item-inner">
-                        <div class="item-title" v-if="!userID">Select User</div>
-                        <div class="item-after" :class="{'pz-max-width100': userID, 'pz-width100': userID}"></div>
-                    </div>
-                </a>
-            </div>
-        </section>
+        <user-select :value.sync="userID" />
 
-        <section v-if="users && userID">
+        <section v-if="userID">
 
             <div class="list-block">
                 <ul>
@@ -103,6 +93,7 @@ import ImageUploader from "../shared/ImageUploader";
 import FilterDateRange from "../FilterDateRange";
 import FilterSingleSelect from "../FilterSingleSelect";
 import FilterSearch from "../FilterSearch";
+import UserSelect from "../shared/UserSelect";
 
 export default {
     name: "InitiateReturn",
@@ -156,62 +147,10 @@ export default {
         "image-uploader": ImageUploader,
         "date-range": FilterDateRange,
         "single-select": FilterSingleSelect,
-        "text-search": FilterSearch
+        "text-search": FilterSearch,
+        "user-select": UserSelect
     },
     methods: {
-        getAllUsers() {
-            window.vm.$http
-                .get(window._pz.apiEndPt + "users/list?type=2")
-                .then(res => {
-                    console.log("res: ", res);
-                    if (res.ok) {
-                        this.users = res.body;
-                        this.setUserSelection();
-                    }
-                })
-                .catch(window._pz.errFunc2.bind(this));
-        },
-        setUserSelection() {
-            let that = this; // save a ref to this to use inside callbacks
-            this.autocompleteRef = window.vm.$f7.autocomplete({
-                openIn: "popup", //open in popup
-                opener: window.vm.Dom7("#autocomplete-standalone-popup-2"), //link that opens autocomplete
-                backOnSelect: true, //go back after we select something
-                textProperty: "name",
-                autoFocus: true,
-                valueProperty: "id",
-                source(autocomplete, query, render) {
-                    if (query.length < 3) return;
-                    let results = [];
-                    if (query && query.length === 0) {
-                        render(results);
-                        return;
-                    }
-                    // Find matched items
-                    for (var i = 0; i < that.users.length; i++) {
-                        if (that.users[i].name.toLowerCase().indexOf(query.toLowerCase()) >= 0) results.push(that.users[i]);
-                    }
-                    // Render items by passing array with result items
-                    render(results);
-                },
-                onChange(autocomplete, value) {
-                    // Add item text value to item-after
-                    window.vm
-                        .Dom7("#autocomplete-standalone-popup-2")
-                        .find(".item-after")
-                        .text(value[0].name);
-                    // Add item value to input value
-                    that.userID = value[0].id;
-                },
-                onClose() {
-                    window.f7.params.hideNavbarOnPageScroll = true;
-                }
-            });
-        },
-        openUserSelection() {
-            window.f7.params.hideNavbarOnPageScroll = false;
-            this.autocompleteRef.open();
-        },
         getWarehouses() {
             window.vm.$http
                 .get(window._pz.apiEndPt + "inventory/warehouses")
@@ -275,7 +214,6 @@ export default {
     },
     created() {
         console.debug(this.$options.name + " created");
-        this.getAllUsers();
         this.getWarehouses();
     },
     beforeMount() {
---
+++ src/components/shared/UserSelect.vue
@@ -0,0 +1,108 @@
+<template>
+    <div class="list-block pz-margin-top0">
+        <a href="#" id="autocomplete-standalone-popup-2" class="item-link item-content autocomplete-opener" @click="openUserSelection()">
+            <input type="hidden" v-model="userID">
+            <div class="item-inner">
+                <div class="item-title" v-if="!userID">Select User</div>
+                <div class="item-after" :class="{'pz-max-width100': userID, 'pz-width100': userID}"></div>
+            </div>
+        </a>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "UserSelect",
+    data() {
+        return {
+            users: null,
+            userID: null
+        };
+    },
+    watch: {
+        userID() {
+            this.$emit('update:value', this.userID);
+        }
+    },
+    methods: {
+        getAllUsers() {
+            window.vm.$http
+                .get(window._pz.apiEndPt + "users/list?type=2")
+                .then(res => {
+                    console.log("res: ", res);
+                    if (res.ok) {
+                        this.users = res.body;
+                        this.setUserSelection();
+                    }
+                })
+                .catch(window._pz.errFunc2.bind(this));
+        },
+        setUserSelection() {
+            let that = this; // save a ref to this to use inside callbacks
+            this.autocompleteRef = window.vm.$f7.autocomplete({
+                openIn: "popup", //open in popup
+                opener: window.vm.Dom7("#autocomplete-standalone-popup-2"), //link that opens autocomplete
+                backOnSelect: true, //go back after we select something
+                textProperty: "name",
+                autoFocus: true,
+                valueProperty: "id",
+                source(autocomplete, query, render) {
+                    if (query.length < 3) return;
+                    let results = [];
+                    if (query && query.length === 0) {
+                        render(results);
+                        return;
+                    }
+                    // Find matched items
+                    for (var i = 0; i < that.users.length; i++) {
+                        if (that.users[i].name.toLowerCase().indexOf(query.toLowerCase()) >= 0) results.push(that.users[i]);
+                    }
+                    // Render items by passing array with result items
+                    render(results);
+                },
+                onChange(autocomplete, value) {
+                    // Add item text value to item-after
+                    window.vm
+                        .Dom7("#autocomplete-standalone-popup-2")
+                        .find(".item-after")
+                        .text(value[0].name);
+                    // Add item value to input value
+                    that.userID = value[0].id;
+                },
+                onClose() {
+                    window.f7.params.hideNavbarOnPageScroll = true;
+                }
+            });
+        },
+        openUserSelection() {
+            window.f7.params.hideNavbarOnPageScroll = false;
+            this.autocompleteRef.open();
+        }
+    },
+    beforeCreate() {
+        console.debug(this.$options.name + " beforeCreate");
+    },
+    created() {
+        console.debug(this.$options.name + " created");
+        this.getAllUsers();
+    },
+    beforeMount() {
+        console.debug(this.$options.name + " beforeMount");
+    },
+    mounted() {
+        console.debug(this.$options.name + " mounted");
+    },
+    beforeUpdate() {
+        console.debug(this.$options.name + " beforeUpdate");
+    },
+    updated() {
+        console.debug(this.$options.name + " updated");
+    },
+    beforeDestroy() {
+        console.debug(this.$options.name + " beforeDestroy");
+    },
+    destroyed() {
+        console.debug(this.$options.name + " destroyed");
+    }
+};
+</script>

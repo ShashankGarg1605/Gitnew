8275e826b702addab0a76c6f46ede8a72ac094e0
Abhishek Jain <abhishek.jain@prozo.com>
2018-02-09 18:23:25 +0530

    migration to user select component

--- src/components/pages/PlaceImageOrder.vue
+++ src/components/pages/PlaceImageOrder.vue
@@ -7,35 +7,25 @@
             </f7-nav-center>
         </f7-navbar>
 
-        <section v-if="users">
-            <div class="list-block pz-margin-top0">
-                <a href="#" id="autocomplete-standalone-popup-2" class="item-link item-content autocomplete-opener" @click="openUserSelection()">
-                    <input type="hidden" v-model="userID">
-                    <div class="item-inner">
-                        <div class="item-title" v-if="!userID">Select User</div>
-                        <div class="item-after" :class="{'pz-max-width100': userID, 'pz-width100': userID}"></div>
-                    </div>
-                </a>
-            </div>
-        </section>
+        <user-select :value.sync="userID" />
 
-        <section v-if="users && userID" style="width: 80%;margin: 0px auto;">
+        <section v-if="userID" style="width: 80%;margin: 0px auto;">
             <image-uploader :maxCount="5" :submitLabel="'Place Image Order'" :inputTitles="true" :tooltip="true" @upload="uploadImage($event)" />
         </section>
 
-        <div class="color-gray pz-page-err" v-if="!users && !$pzGlobalReactiveData.pendingReq">{{errMsg}}</div>
+        <div class="color-gray pz-page-err" v-if="!$pzGlobalReactiveData.pendingReq">{{errMsg}}</div>
 
     </f7-page>
 </template>
 
 <script>
 import ImageUploader from '../shared/ImageUploader';
+import UserSelect from "../shared/UserSelect";
 
 export default {
     name: 'PlaceImageOrder',
     data() {
         return {
-            users: null,
             userID: null,
             errMsg: null,
             autocompleteRef: null,
@@ -43,61 +33,13 @@ export default {
         };
     },
     components: {
-        'image-uploader': ImageUploader
+        'image-uploader': ImageUploader,
+        "user-select": UserSelect
     },
     methods: {
-        getAllUsers() {
-            window.vm.$http.get(window._pz.apiEndPt + 'users/list?type=2')
-                .then(res => {
-                    console.log('res: ', res);
-                    if (res.ok) {
-                        this.users = res.body;
-                        this.setUserSelection();
-                    }
-                })
-                .catch(window._pz.errFunc2.bind(this));
-        },
-        setUserSelection() {
-            let that = this; // save a ref to this to use inside callbacks
-            this.autocompleteRef = window.vm.$f7.autocomplete({
-                openIn: 'popup', //open in popup
-                opener: window.vm.Dom7('#autocomplete-standalone-popup-2'), //link that opens autocomplete
-                backOnSelect: true, //go back after we select something
-                textProperty: 'name',
-                autoFocus: true,
-                valueProperty: 'id',
-                source(autocomplete, query, render) {
-                    if (query.length < 3) return;
-                    let results = [];
-                    if (query && query.length === 0) {
-                        render(results);
-                        return;
-                    }
-                    // Find matched items
-                    for (var i = 0; i < that.users.length; i++) {
-                        if (that.users[i].name.toLowerCase().indexOf(query.toLowerCase()) >= 0) results.push(that.users[i]);
-                    }
-                    // Render items by passing array with result items
-                    render(results);
-                },
-                onChange(autocomplete, value) {
-                    // Add item text value to item-after
-                    window.vm.Dom7('#autocomplete-standalone-popup-2').find('.item-after').text(value[0].name);
-                    // Add item value to input value
-                    that.userID = value[0].id;
-                },
-                onClose() {
-                    window.f7.params.hideNavbarOnPageScroll = true;
-                }
-            });
-        },
-        openUserSelection() {
-            window.f7.params.hideNavbarOnPageScroll = false;
-            this.autocompleteRef.open();
-        },
         uploadImage(images) {
             window.vm.$f7.showPreloader();
-            
+
             let params = images.map(image => ({
                 stringValue: image.data,
                 name: image.title
@@ -123,10 +65,7 @@ export default {
         }
     },
     beforeCreate() { console.debug(this.$options.name + ' beforeCreate'); },
-    created() {
-        console.debug(this.$options.name + ' created');
-        this.getAllUsers();
-    },
+    created() { console.debug(this.$options.name + ' created'); },
     beforeMount() { console.debug(this.$options.name + ' beforeMount'); },
     mounted() { console.debug(this.$options.name + ' mounted'); },
     beforeUpdate() { console.debug(this.$options.name + ' beforeUpdate'); },

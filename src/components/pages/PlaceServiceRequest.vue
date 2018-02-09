14c85ad0ee726717820dd8075e2074624975470a
Abhishek Jain <abhishek.jain@prozo.com>
2018-02-09 18:26:08 +0530

    migration to user select component

--- src/components/pages/PlaceServiceRequest.vue
+++ src/components/pages/PlaceServiceRequest.vue
@@ -10,19 +10,9 @@
       </f7-nav-center>
     </f7-navbar>
 
-    <section v-if="users">
-      <div class="list-block pz-margin-top0">
-        <a href="#" id="autocomplete-standalone-popup-2" class="item-link item-content autocomplete-opener" @click="openUserSelection()">
-          <input type="hidden" v-model="userID">
-          <div class="item-inner">
-            <div class="item-title" v-if="!userID">Select User</div>
-            <div class="item-after" :class="{'pz-max-width100': userID, 'pz-width100': userID}"></div>
-          </div>
-        </a>
-      </div>
-    </section>
+    <user-select :value.sync="userID" />
 
-    <section v-if="users && userID">
+    <section v-if="userID">
 
       <div class="list-block">
         <ul>
@@ -56,7 +46,7 @@
 
     </section>
 
-    <div class="color-gray pz-page-err" v-if="!users && !$pzGlobalReactiveData.pendingReq">{{errMsg}}</div>
+    <div class="color-gray pz-page-err" v-if="!$pzGlobalReactiveData.pendingReq">{{errMsg}}</div>
 
   </f7-page>
 </template>
@@ -96,12 +86,12 @@ textarea {
 <script>
 import ImageUploader from "../shared/ImageUploader";
 import FilterSingleSelect from "../FilterSingleSelect";
+import UserSelect from "../shared/UserSelect";
 
 export default {
   name: "PlaceServiceRequest",
   data() {
     return {
-      users: null,
       userID: null,
       errMsg: null,
       autocompleteRef: null,
@@ -113,62 +103,10 @@ export default {
   },
   components: {
     "image-uploader": ImageUploader,
-    "single-select": FilterSingleSelect
+    "single-select": FilterSingleSelect,
+    "user-select": UserSelect
   },
   methods: {
-    getAllUsers() {
-      window.vm.$http
-        .get(window._pz.apiEndPt + "users/list?type=2")
-        .then(res => {
-          console.log("res: ", res);
-          if (res.ok) {
-            this.users = res.body;
-            this.setUserSelection();
-          }
-        })
-        .catch(window._pz.errFunc2.bind(this));
-    },
-    setUserSelection() {
-      let that = this; // save a ref to this to use inside callbacks
-      this.autocompleteRef = window.vm.$f7.autocomplete({
-        openIn: "popup", //open in popup
-        opener: window.vm.Dom7("#autocomplete-standalone-popup-2"), //link that opens autocomplete
-        backOnSelect: true, //go back after we select something
-        textProperty: "name",
-        autoFocus: true,
-        valueProperty: "id",
-        source(autocomplete, query, render) {
-          if (query.length < 3) return;
-          let results = [];
-          if (query && query.length === 0) {
-            render(results);
-            return;
-          }
-          // Find matched items
-          for (var i = 0; i < that.users.length; i++) {
-            if (that.users[i].name.toLowerCase().indexOf(query.toLowerCase()) >= 0) results.push(that.users[i]);
-          }
-          // Render items by passing array with result items
-          render(results);
-        },
-        onChange(autocomplete, value) {
-          // Add item text value to item-after
-          window.vm
-            .Dom7("#autocomplete-standalone-popup-2")
-            .find(".item-after")
-            .text(value[0].name);
-          // Add item value to input value
-          that.userID = value[0].id;
-        },
-        onClose() {
-          window.f7.params.hideNavbarOnPageScroll = true;
-        }
-      });
-    },
-    openUserSelection() {
-      window.f7.params.hideNavbarOnPageScroll = false;
-      this.autocompleteRef.open();
-    },
     getReasons() {
       window.vm.$http
         .get(window._pz.apiEndPt + "utils/reasons?type=7")
@@ -187,10 +125,6 @@ export default {
       console.log("imadeAdded: ", images);
       // just refer to the image uploader component's image array. Removing will happen automatically then.
       this.images = images;
-    },
-    imadeRemoved(images) {
-      console.log("imadeRemoved: ", images);
-      // no need to manually remove images from this page's props, since this.images directly references the image uploader component's images array
     },
     submit(images) {
       window.vm.$f7.showPreloader();
@@ -231,7 +165,6 @@ export default {
   },
   created() {
     console.debug(this.$options.name + " created");
-    this.getAllUsers();
     this.getReasons();
   },
   beforeMount() {

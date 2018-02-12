7097f673407e88dc5d64101b764d54e4d15b9689
Abhishek Jain <abhishek.jain@prozo.com>
2018-02-12 23:04:20 +0530

    pwa and other configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,45 +1,43 @@
 module.exports = {
   root: true,
-  parser: 'babel-eslint',
+  parser: "babel-eslint",
   parserOptions: {
-    sourceType: 'module'
+    sourceType: "module"
   },
-  extends: 'semistandard',
+  extends: "semistandard",
   env: {
     browser: true,
     node: true,
     mocha: true,
-    jasmine: true,
+    jasmine: true
   },
   // required to lint *.vue files
-  plugins: [
-    'html'
-  ],
+  plugins: ["html"],
   // check if imports actually resolve
-  'settings': {
-    'import/resolver': {
-      'webpack': {
-        'config': `${__dirname}/build/webpack.base.conf.js`
+  settings: {
+    "import/resolver": {
+      webpack: {
+        config: `${__dirname}/build/webpack.base.conf.js`
       }
     }
   },
   // add your custom rules here
-  'rules': {
-    'no-console': 0,
+  rules: {
+    "no-console": 0,
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
-    'space-before-function-paren': 0,
-    'no-trailing-spaces': 0,
-    'padded-blocks': 0,
-    'prefer-promise-reject-errors': 0,
-    'no-multiple-empty-lines': 0,
-    'eol-last': 0,
-    'indent': 0,
-    'brace-style': 0,
-    'no-extend-native': 0,
-    'spaced-comment': 0,
-    'quotes': 0,
-    'curly': 0,
-    'space-before-blocks': 0
+    "no-debugger": process.env.NODE_ENV === "production" ? 2 : 0,
+    "space-before-function-paren": 0,
+    "no-trailing-spaces": 0,
+    "padded-blocks": 0,
+    "prefer-promise-reject-errors": 0,
+    "no-multiple-empty-lines": 0,
+    "eol-last": 0,
+    indent: 0,
+    "brace-style": 0,
+    "no-extend-native": 0,
+    "spaced-comment": 0,
+    quotes: 0,
+    curly: 0,
+    "space-before-blocks": 0
   }
-}
+};

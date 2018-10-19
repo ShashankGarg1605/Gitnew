c9ea5085b65cfcf27b3bf901e9ddfc92022fd2d1
Abhishek Jain (MBP) <ajain@bluestacks.com>
2018-10-19 19:07:50 +0530

    link for viewing all orders for verification

--- src/components/pages/OrderVerification/OrderSearch.vue
+++ src/components/pages/OrderVerification/OrderSearch.vue
@@ -12,18 +12,23 @@
     <main>
       <form @submit.prevent="onSubmit">
         <div>
+          <div class="content-block-title">You can apply multiple filters at once</div>
           <label for="order-id">Enter Order ID:</label>
           <input type="number" v-model="orderId" name="order-id" placeholder="Last few digits">
         </div>
         <button submit class="button button-fill button-raised color-teal">Search</button>
       </form>
+      <button
+        class="view-verified"
+        @click="openOrdersForVerification()"
+      >View all orders for verification</button>
     </main>
   </f7-page>
 </template>
 
 <style scoped>
 form {
-  padding: 50px 20px;
+  padding: 50px 20px 0px 20px;
 }
 
 form > div {
@@ -43,6 +48,17 @@ input {
 button {
   margin: 20px auto;
   padding: 0px 40px;
+}
+
+.view-verified {
+  text-decoration: underline;
+  color: #5f7c8b;
+  font-weight: bold;
+  text-transform: capitalize;
+  width: 100%;
+  text-align: right;
+  font-size: 14px;
+  padding: 15px 20px 10px 0px;
 }
 </style>
 
@@ -66,6 +82,21 @@ export default {
     };
   },
   methods: {
+openOrdersForVerification() {
+     window.vm.$http
+        .get(`${window._pz.apiEndPt}orders?orderBy=created_date&orderByValue=desc&limit=100&offset=0&status=103`)
+        .then(res => {
+          if (res.ok) {
+            console.log("res.body: ", res.body);
+            const orders = res.body;
+            window.vm.$f7.mainView.router.load({
+              url: "OrderSelect",
+              context: { orders }
+            });
+          }
+        })
+        .catch(window._pz.errFunc2.bind(this));
+},
     onSubmit(e) {
       console.log(this.orderId);
       if (this.orderId) this.fetchOrders();
@@ -84,7 +115,6 @@ export default {
           }
         })
         .catch(window._pz.errFunc2.bind(this));
-
     }
   },
   beforeCreate() {

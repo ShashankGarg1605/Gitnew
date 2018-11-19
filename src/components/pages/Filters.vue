be208aabd26d0a26adc6dc5ce24bcc890c9d6f17
Abhishek Jain <abhishek.jain@prozo.com>
2018-11-19 22:37:09 +0530

    code formatting the file

--- src/components/pages/Filters.vue
+++ src/components/pages/Filters.vue
@@ -1,17 +1,19 @@
 <template>
   <f7-page name="Filters">
-
     <f7-navbar back-link="Back" sliding>
-      <f7-nav-center>
-        Filters
-      </f7-nav-center>
+      <f7-nav-center>Filters</f7-nav-center>
     </f7-navbar>
     <div class="content-block-title">You can apply multiple filters at once</div>
     <!-- <span style="font-size: xx-small;">{{comps}}</span> -->
     <div class="list-block">
       <ul>
         <li>
-          <user-select :value.sync="comps.userSelect" :default="comps.userSelect" v-if="comps.userSelect !== undefined" class="pz-margin-0" />
+          <user-select
+            :value.sync="comps.userSelect"
+            :default="comps.userSelect"
+            v-if="comps.userSelect !== undefined"
+            class="pz-margin-0"
+          />
         </li>
         <!-- <li class="item-content button pz-colr-inherit pz-cap">
           <div class="item-media">
@@ -22,19 +24,41 @@
               <user-select :value.sync="comps.userSelect" v-if="comps.userSelect !== undefined" class="pz-margin-0" />
             </div>
           </div>
-        </li> -->
-        <pz-filter-date-range v-model="comp.value" v-for="(comp, index) in comps.date" :key="index" :placeholder="comp.placeholder" />
-        <pz-filter-search :value.sync="comp.value" v-for="(comp, index) in comps.search" :key="index" :placeholder="comp.placeholder" />
-        <pz-filter-single-select :value.sync="comp.value" v-for="(comp, index) in comps.singleselect" :key="index" :placeholder="comp.placeholder" :opts="comp.opts" />
+        </li>-->
 
+        <pz-filter-date-range
+          v-model="comp.value"
+          v-for="(comp, index) in comps.date"
+          :key="index"
+          :placeholder="comp.placeholder"
+        />
+        <pz-filter-search
+          :value.sync="comp.value"
+          v-for="(comp, index) in comps.search"
+          :key="index"
+          :placeholder="comp.placeholder"
+        />
+        <pz-filter-single-select
+          :value.sync="comp.value"
+          v-for="(comp, index) in comps.singleselect"
+          :key="index"
+          :placeholder="comp.placeholder"
+          :opts="comp.opts"
+        />
       </ul>
     </div>
-
     <div class="bottom">
-      <a href="#" class="button color-green pz-margin-r16 pz-padding-lr32" @click="resetFilters()">Reset</a>
-      <a href="#" class="button button-fill button-raised color-teal pz-margin-r16 pz-padding-lr32" @click="applyFilters()">Apply</a>
+      <a
+        href="#"
+        class="button color-green pz-margin-r16 pz-padding-lr32"
+        @click="resetFilters()"
+      >Reset</a>
+      <a
+        href="#"
+        class="button button-fill button-raised color-teal pz-margin-r16 pz-padding-lr32"
+        @click="applyFilters()"
+      >Apply</a>
     </div>
-
   </f7-page>
 </template>
 
